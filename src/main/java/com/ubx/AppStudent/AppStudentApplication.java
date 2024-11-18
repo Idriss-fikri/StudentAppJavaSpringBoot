1331e96779c19c395ecb0721cdd9b08a10936f53
Idriss-Fikri <fikri.idriss12@gmail.com>
2024-11-18 14:54:04 +0100

    init student project

---
+++ src/main/java/com/ubx/AppStudent/AppStudentApplication.java
@@ -0,0 +1,13 @@
+package com.ubx.AppStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStudentApplication.class, args);
+	}
+
+}
